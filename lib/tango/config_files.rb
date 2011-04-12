c638f210f23826cb309bbbce26a96be8c6ced00c
Lucas Parry <lucas@envato.com>
2011-04-12 16:50:08 +1000

    write with file permissions

--- lib/tango/config_files.rb
+++ lib/tango/config_files.rb
@@ -1,12 +1,14 @@
 require 'erb'
+require 'fileutils'
 
 module Tango
   module ConfigFiles
 
-    def write(path, contents)
+    def write(path, contents, permissions = nil)
       contents = unindent(contents)
       contents = ERB.new(contents).result(binding)
       File.open(path, "w") {|file| file.write(contents) }
+      FileUtils.chmod(permissions, path) if permissions
     end
 
   private
