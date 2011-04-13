f5b04ee3d42ba1045748c9042b552291e0f9eef4
Pete Yandell <pete@notahat.com>
2011-04-14 01:34:43 +1000

    Nicer specs for contexts.

---
+++ spec/contexts/chain_spec.rb
@@ -0,0 +1,75 @@
+require 'tango'
+
+module Tango::Contexts
+  describe Chain do
+
+    before do
+      @context_class = Class.new do
+        def in_context?
+          @in_context
+        end
+
+        def initialize
+          @in_context = false
+        end
+
+        def enter 
+          @in_context = true
+        end
+
+        def exit
+          @in_context = false
+        end
+      end
+
+      @context = @context_class.new
+    end
+
+    it "should run a block" do
+      block_run = false
+      Chain.new.in_context(@context) { block_run = true }
+      block_run.should be_true
+    end
+
+    it "should return the return value of the block" do
+      result = Chain.new.in_context(@context) { "I woz ere." }
+      result.should == "I woz ere."
+    end
+
+    it "should run the enter method before the block" do
+      Chain.new.in_context(@context) do
+        @context.should be_in_context
+      end
+    end
+
+    it "should run the exit method after leaving the block" do
+      Chain.new.in_context(@context) { }
+      @context.should_not be_in_context
+    end
+
+    it "should run the exit method on an exception" do
+      expect {
+        Chain.new.in_context(@context) { raise "Uh oh" }
+      }.should raise_error("Uh oh")
+      @context.should_not be_in_context
+    end
+
+    it "should run the enter method of each context in a chain" do
+      a = @context_class.new
+      b = @context_class.new
+      Chain.new.in_context(a).in_context(b) do
+        a.should be_in_context
+        b.should be_in_context
+      end
+    end
+
+    it "should run the exit method of each context in a chain" do
+      a = @context_class.new
+      b = @context_class.new
+      Chain.new.in_context(a).in_context(b) { }
+      a.should_not be_in_context
+      b.should_not be_in_context
+    end
+
+  end
+end
--- spec/contexts/directory_spec.rb
+++ spec/contexts/directory_spec.rb
@@ -17,18 +17,7 @@ module Tango::Contexts
       Dir.chdir(@original_directory)
     end
 
-    it "should run a block" do
-      block_run = false
-      @stub.in_directory("/tmp") { block_run = true }
-      block_run.should be_true
-    end
-
-    it "should return the return value of the block" do
-      result = @stub.in_directory("/tmp") { "I woz ere." }
-      result.should == "I woz ere."
-    end
-
-    it "should change directory" do
+    it "should change the working directory" do
       directory = Dir.getwd + "/lib"
       @stub.in_directory(directory) do
         Dir.getwd.should == directory
@@ -39,15 +28,6 @@ module Tango::Contexts
       old_directory = Dir.getwd
       directory = Dir.getwd + "/lib"
       @stub.in_directory(directory) { }
-      Dir.getwd.should == old_directory
-    end
-
-    it "should restore the original directory after an exception" do
-      old_directory = Dir.getwd
-      directory = Dir.getwd + "/lib"
-      expect {
-        @stub.in_directory(directory) { raise "Uh oh!" }
-      }.should raise_error
       Dir.getwd.should == old_directory
     end
 
