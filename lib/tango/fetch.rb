cdd37546ddd77dcbf062d618c3badad1c7dee66c
Pete Yandell <pete@notahat.com>
2011-10-30 09:56:18 +1100

    Make fetch try wget, and fall back to curl.

--- lib/tango/fetch.rb
+++ lib/tango/fetch.rb
@@ -2,7 +2,17 @@ module Tango
   module Fetch
 
     def fetch(url)
-      shell("curl", "-O", "-C", "-", "-L", "--progress-bar", url)
+      if have_wget?
+        shell("wget", "--progress=bar:force", "--continue", url)
+      else
+        shell("curl", "-O", "-C", "-", "-L", "--progress-bar", url)
+      end
+    end
+
+  private
+
+    def have_wget?
+      shell("which", "wget", :echo => false).succeeded?
     end
 
   end
