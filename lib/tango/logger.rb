6403862148417ec2d256e0508e34ea8ad729b163
Ian Leitch <port001@gmail.com>
2012-02-14 20:41:15 +0000

    Add a little colour to the dance.

--- lib/tango/logger.rb
+++ lib/tango/logger.rb
@@ -1,7 +1,20 @@
 # coding: utf-8
 
 module Tango
+  module TermANSIColorStubs
+    [:red, :green, :yellow].each do |color|
+      define_method(color) { |str| str }
+    end
+  end
+
   class Logger
+    begin
+      require 'term/ansicolor'
+      include Term::ANSIColor
+    rescue LoadError
+      include TermANSIColorStubs
+    end
+
     def self.instance
       @logger ||= Logger.new
     end
@@ -12,18 +25,18 @@ module Tango
     end
 
     def begin_step(step_name)
-      log "#{step_name} {"
+      log "#{yellow(step_name)} {"
       @depth += 1
     end
 
     def step_met(step_name)
       @depth -= 1
-      log "} √ #{step_name}"
+      log "} #{green("√ #{step_name}")}"
     end
 
     def step_not_met(step_name)
       @depth -= 1
-      log "} ✕ #{step_name}\n\n"
+      log "} #{red("✕ #{step_name}")}\n\n"
     end
 
     def log(message)
